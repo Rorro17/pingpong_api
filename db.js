8cadbde875fa425beeeeab2029a8603c260fc641
Rodrigo Manrique <romanrique86@gmail.com>
2017-06-30 10:17:07 -0300

    Changed DB and packaje

--- db.js
+++ db.js
@@ -1,3 +1,6 @@
 // db.js
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:3306/pinpon');
+
+mongoose.connect('mongodb://admin:QU!yupZ3pnmbF4^115@ds123050.mlab.com:23050/pinpon');
+//mongoose.connect('mongodb://localhost:3306/pinpon');
+
